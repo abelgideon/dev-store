e196a71942ba8bb5938563fa52ba8813bee23ada
Abel Gideon <abelgideontk7@gmail.com>
2026-04-12 23:27:29 +0300

    Design checkout page

---
+++ src/components/CheckoutItem.jsx
@@ -0,0 +1,10 @@
+function CheckoutItem() {
+  return (
+    <div className="checkout-item">
+      <p className="checkout-item-name">Keyboard X1</p>
+      <p className="checkout-item-price">$12.32</p>
+    </div>
+  );
+}
+
+export default CheckoutItem;
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,72 @@
+import { Link } from "react-router-dom";
+import CheckoutItem from "../components/CheckoutItem";
+
 function CheckoutPage() {
-  return <h1>This is the checkout page</h1>;
+  return (
+    <>
+      <h1 className="checkout-header">Checkout</h1>
+      <p className="checkout-sub">
+        Enter personal information and review order
+      </p>
+
+      <form className="checkout-form">
+        <div>
+          <label className="checkout-form-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            placeholder="Abebe Kebede"
+            type="text"
+            name="name"
+            id="name"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="checkout-form-label" htmlFor="email">
+            Email
+          </label>
+          <input
+            placeholder="example@gmail.com"
+            type="email"
+            name="email"
+            id="email"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="checkout-form-label" htmlFor="address">
+            Address
+          </label>
+          <input
+            placeholder="Addis Ababa"
+            type="text"
+            name="address"
+            id="address"
+            required
+          />
+        </div>
+
+        <div>
+          <p className="checkout-form-label">Items</p>
+          <div className="checkout-items">
+            <CheckoutItem />
+            <CheckoutItem />
+            <CheckoutItem />
+          </div>
+        </div>
+
+        <h1>
+          Total: <span className="checkout-price">$100.29</span>
+        </h1>
+        <Link to="/orders" className="btn checkout-confirm-btn">
+          Confirm
+        </Link>
+      </form>
+    </>
+  );
 }
 
 export default CheckoutPage;
