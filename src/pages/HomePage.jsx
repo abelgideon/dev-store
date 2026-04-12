f4141a40e6cdca783a52f5f84aa695877bd2a30e
Abel Gideon <abelgideontk7@gmail.com>
2026-04-12 14:11:28 +0300

    Design the Home page
    To make the hero section fit better on smaller devices at a given break
    point the second line of the hero header is set to none and coloring is
    applied to the last word of the first line of the hero header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>This is the home page</h1>;
+  return (
+    <div className="hero-section">
+      <h1 className="hero-header">
+        Build Better <span className="hero-header-last">Projects</span>
+        <br />
+        <span className="hero-second-line">With the Right Tools</span>
+      </h1>
+      <p className="hero-para">
+        Get everything you need to start your developer journey all in one place
+      </p>
+      <Link to="/products" className="btn hero-btn font-sm">
+        Start Browsing
+      </Link>
+    </div>
+  );
 }
 
 export default HomePage;
