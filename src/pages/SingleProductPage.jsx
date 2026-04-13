d23847a761e0c436fdac2ca6dbc90d272c8d7868
Abel Gideon <abelgideontk7@gmail.com>
2026-04-13 16:53:42 +0300

    Make product details page dynamic
    This doesn't include the implementation for the Add to cart button but
    includes updating the information based on which product was clicked
    from the products section.

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,24 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import products from "../products";
 
 function SingleProductPage() {
+  const { id } = useParams();
+  const product = products.find((product) => product.id === Number(id));
+
   return (
     <div className="single-product-container">
-      <img className="single-product-img" src="/products/keyboard.jpg" alt="" />
+      <img
+        className="single-product-img"
+        src={product.image}
+        alt={product.name}
+      />
       <div className="single-product-right">
-        <h1>KEYBOARD</h1>
-        <p className="single-product-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-          laborum laboriosam pariatur asperiores assumenda illo dolor aspernatur
-          quam reprehenderit aliquam consequuntur dignissimos et tempore quas
-          eius tenetur nostrum eveniet necessitatibus voluptate, esse explicabo
-          at blanditiis enim! Consequatur corporis nulla maxime quisquam sunt
-          dignissimos quaerat voluptas nam repellendus quam! Adipisci incidunt
-          autem reprehenderit laborum, fuga fugit architecto velit? Excepturi,
-          dolor debitis.
-        </p>
+        <h1>{product.name}</h1>
+        <p className="single-product-description">{product.description}</p>
         <div className="single-product-footer">
-          <p className="single-product-stock">Stock: available</p>
-          <p className="single-product-price">$12.33</p>
+          <p className="single-product-stock">
+            Stock: {product.stock > 0 ? product.stock : "Out of stock"}
+          </p>
+          <p className="single-product-price">${product.price}</p>
         </div>
         <Link className="btn cart-btn" to="/cart">
           Add to cart
